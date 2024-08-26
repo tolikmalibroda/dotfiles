d8ca20eee30f0c71fa78bcf43c53ce906ff3eee3
Tolik Malibroda <tolikmalibroda@gmail.com>
2024-08-26 10:17:17 +0200

    Added neovim config to dotfiles.

---
+++ nvim/dot-config/nvim/lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>ft', ':Neotree filesystem reveal left<CR>', {})
+  end,
+}
---
+++ nvim/dot-config/nvim/lua/custom/plugins/vim-fugitive.lua
@@ -0,0 +1,4 @@
+-- Adds :Git or :G for execution git commands
+return {
+  'tpope/vim-fugitive',
+}
