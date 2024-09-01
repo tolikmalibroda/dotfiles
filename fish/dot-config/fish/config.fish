33210e25a9ee183e9d5fa52adc14af0ce71401bc
Tolik Malibroda <tolikmalibroda@gmail.com>
2024-09-01 19:50:37 +0200

    Using default node on shell start

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   eval "$(/opt/homebrew/bin/brew shellenv)"
   starship init fish | source
+  nvm use default --silent
 end
 
