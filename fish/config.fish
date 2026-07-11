94e5d974d165c742ffeb0c7713a8f5a3d8856d88
Tolik Malibroda <tolikmalibroda@gmail.com>
2026-07-11 21:40:00 +0200

    Update fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,11 @@ if status is-interactive
   eval "$(/opt/homebrew/bin/brew shellenv)"
   starship init fish | source
   atuin init fish | source
+  zoxide init fish | source
   set -gx EDITOR nvim
 end
 
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/Users/tolikmalibroda/.local/bin" $PATH
